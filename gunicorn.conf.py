fde8cabf140c37e9601c9de5e2a8876346d609f6
hannaguptabc <hanna.gupta@blenheimchalcot.com>
2024-03-13 16:28:43 +0530

    multiprocessing configuration

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,15 @@
-# Gunicorn configuration file
 import multiprocessing
 
 max_requests = 1000
 max_requests_jitter = 50
 
-log_file = "-"
+log_file = "-"  # Output logs to stdout
 
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:8000"  # Bind to all network interfaces on port 8000
 
-worker_class = "uvicorn.workers.UvicornWorker"
-workers = (multiprocessing.cpu_count() * 2) + 1
+worker_class = "gthread"  # Use the gthread worker class
+threads = multiprocessing.cpu_count() * 2  # Number of threads = 2 * number of CPU cores
+
+workers = (multiprocessing.cpu_count() * 2) + 1  # Number of Gunicorn worker processes
+
+
